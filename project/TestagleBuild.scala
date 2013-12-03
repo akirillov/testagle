1b7ea14f4207a446ba79c85cf685594b276b2ad1
Anton Kirillov <akirillov@zeptolab.com>
2013-12-03 17:06:05 +0400

    (+) finagle dependencies added; (~) minor structural changes

--- project/TestagleBuild.scala
+++ project/TestagleBuild.scala
@@ -3,8 +3,18 @@ import Keys._
 
 object HelloBuild extends Build {
 
-	lazy val root = project.in(file(".")).aggregate(core, api)
 
-	lazy val core = project.in(file("testagle-core")).dependsOn(api)
+
+	val finaglecore = "com.twitter" %% "finagle-core" % "6.8.1"
+
+
+	lazy val testagle = project.in(file(".")).aggregate(core, api)
+
+	lazy val core = project.in(file("testagle-core")).dependsOn(api).settings(
+		name := "testagle-core",
+		libraryDependencies ++= Seq(finaglecore)
+	)
+
 	lazy val api = project.in(file("testagle-api"))
 }
+
