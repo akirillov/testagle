212767636dca5695bb987e4e25413a01d7ec15d5
Anton Kirillov <akirillov@zeptolab.com>
2013-12-05 12:57:56 +0400

    (+) sbt-scalabuff plugin integrated

--- project/TestagleBuild.scala
+++ project/TestagleBuild.scala
@@ -5,11 +5,11 @@ import scalabuff.ScalaBuffPlugin._
 object TestagleBuild extends Build {
 
 	val finaglecore = "com.twitter" %% "finagle-core" % "6.8.1"
-  val scalabuf = "net.sandrogrzicic" %% "scalabuff-runtime" % "1.3.6"
+  val scalabuff = "net.sandrogrzicic" %% "scalabuff-runtime" % "1.3.6"
 
-  lazy val root = Project(id = "testagle", base = file("."), settings = Defaults.defaultSettings ++ scalabuffSettings) configs(ScalaBuff) aggregate (core, api) dependsOn (core, api)
+  lazy val root = Project(id = "testagle", base = file(".")) aggregate (core, api) dependsOn (core, api)
 
-  lazy val core = Project(id = "testagle-core", base = file("testagle-core")) settings(libraryDependencies ++= Seq(finaglecore, scalabuf), exportJars := true) dependsOn api
+  lazy val core = Project(id = "testagle-core", base = file("testagle-core"), settings = Defaults.defaultSettings ++ scalabuffSettings) configs(ScalaBuff) settings(libraryDependencies ++= Seq(finaglecore), exportJars := true) dependsOn api
 
   lazy val api = Project(id = "testagle-api", base = file("testagle-api")) settings (exportJars := true)
 
